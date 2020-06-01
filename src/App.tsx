6dd8f814fb72b302bf3ae33836878d97c8c2edc2
MongieLee <mongielee@163.com>
2020-06-01 14:50:01 +0800

    使用HashRouter/初步制作项目框架和布局/封装Nav组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
-import React from 'react';
+import React from 'react'
 import styled from 'styled-components'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import Nav from './components/Nav'
 
-const Button = styled.button`color:red;border:none;&:hover{color:green;}`;
-function App() {
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
+
+const NoMatch = styled.div`
+  width: 200px;
+  height: 200px;
+`
+
+function App () {
   return (
-    <div>
-      <Button>nihao</Button>
-    </div>
-  );
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route exact path='/statistics'>
+              <div>statistics</div>
+            </Route>
+            <Route exact path='/money'>
+              asdasdasdas
+            </Route>
+            <Route exact path='/reportForm'>
+              <div>这是repostForm</div>
+            </Route>
+
+            <Redirect exact from='/' to='/money' />
+            <Route path='*'>
+              <NoMatch>页面不存在</NoMatch>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav />
+      </Wrapper>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Nav.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+import React from 'react'
+import {Link} from 'react-router-dom'
+const NavWrapper = styled.ul`
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  li {
+    text-align: center;
+    padding: 16px;
+    width: 33.3333%;
+  }
+`
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <li>
+        <Link to='/'>记账</Link>
+      </li>
+      <li>
+        <Link to='/statistics'>明细</Link>
+      </li>
+      <li>
+        <Link to='/reportForm'>报表</Link>
+      </li>
+    </NavWrapper>
+  )
+}
+
+export default Nav
