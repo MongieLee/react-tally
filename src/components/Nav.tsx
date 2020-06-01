a891c1b717185842138c1e66d30733eb93f6b314
MongieLee <mongielee@163.com>
2020-06-01 17:06:28 +0800

    update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import React from 'react'
 import {Link} from 'react-router-dom'
+import x from 'icons/money.svg'
+console.log(x)
 const NavWrapper = styled.ul`
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -14,7 +16,10 @@ const Nav = () => {
   return (
     <NavWrapper>
       <li>
-        <Link to='/'>记账</Link>
+        <Link to='/'>
+        
+        记账
+        </Link>
       </li>
       <li>
         <Link to='/statistics'>明细</Link>
