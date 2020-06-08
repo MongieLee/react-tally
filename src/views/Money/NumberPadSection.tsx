fad8f569f534f33d320abb8f93056ae3ef454b51
MongieLee <mongielee@163.com>
2020-06-08 15:13:46 +0800

    重构NumberPadSection.tsx

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,31 +1,6 @@
-import styled from 'styled-components'
 import React from 'react'
-const Wrapper = styled.section`
-  .output {
-    background: #fff;
-    line-height: 72px;
-    font-size: 36px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
-      inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
-  }
-  .numberPad {
-    button {
-      font-size: 18px;
-      float: left;
-      width: 25%;
-      height: 64px;
-      &.zero {
-        width: 50%;
-      }
-      &.ok {
-        float: right;
-        height: 128px;
-      }
-    }
-  }
-`
+import { Wrapper } from './NumberPadSection/Wrapper'
+import { generateOutput } from './NumberPadSection/generateOutput'
 const NumberPadSection: React.FC = () => {
   const [output, _setOutput] = React.useState('0')
   const setOutput = (output: string) => {
@@ -39,42 +14,11 @@ const NumberPadSection: React.FC = () => {
   const x = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent
     if (text === null) return
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '0') {
-          setOutput(text)
-        } else {
-          setOutput(output + text)
-        }
-        break
-      case '.':
-        if (output.indexOf('.') >= 0) {
-          return
-        }
-        setOutput(output + '.')
-        break
-      case 'OK':
-        alert('jizhang')
-        break
-      case 'C':
-        setOutput('')
-        break
-      case 'Del':
-        if (output.length === 1) {
-          setOutput('')
-        } else {
-          setOutput(output.slice(0, -1))
-        }
-        break
+    if (text === 'OK') {
+      alert('提交')
+    }
+    if('1234567890.C'.split('').concat(['Del']).indexOf(text)>=0){
+      setOutput(generateOutput(text,output))
     }
   }
   return (
