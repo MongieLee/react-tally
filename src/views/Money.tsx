9cc8e1e664e560186ab1e96e730420cf0eeb9cb8
MongieLee <mongielee@163.com>
2020-06-08 14:11:12 +0800

    numberPad功能基本完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -19,25 +19,8 @@ const Money = () => {
       <CategorySection/>
         
 
-      <NumberPadSection>
-        <div className='output'>100</div>
-        <div className='numberPad clearfix'>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>Del</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>C</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className='ok'>OK</button>
-          <button className='zero'>0</button>
-          <button>.</button>
-        </div>
-      </NumberPadSection>
+      <NumberPadSection />
+        
     </MyLayout>
   )
 }
--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-
-const NumberPadSection = styled.section`
+import React from 'react'
+const Wrapper = styled.section`
   .output {
     background: #fff;
     line-height: 72px;
@@ -26,4 +26,78 @@ const NumberPadSection = styled.section`
     }
   }
 `
+const NumberPadSection: React.FC = () => {
+  const [output, _setOutput] = React.useState('0')
+  const setOutput = (output: string) => {
+    if (output.length > 10) {
+      output = output.slice(0, 10)
+    } else if (output.length === 0) {
+      output = '0'
+    }
+    _setOutput(output)
+  }
+  const x = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent
+    if (text === null) return
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        if (output === '0') {
+          setOutput(text)
+        } else {
+          setOutput(output + text)
+        }
+        break
+      case '.':
+        if (output.indexOf('.') >= 0) {
+          return
+        }
+        setOutput(output + '.')
+        break
+      case 'OK':
+        alert('jizhang')
+        break
+      case 'C':
+        setOutput('')
+        break
+      case 'Del':
+        if (output.length === 1) {
+          setOutput('')
+        } else {
+          setOutput(output.slice(0, -1))
+        }
+        break
+    }
+  }
+  return (
+    <Wrapper>
+      <div className='output'>{output}</div>
+      <div className='numberPad clearfix' onClick={x}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>Del</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>C</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className='ok'>OK</button>
+        <button className='zero'>0</button>
+        <button>.</button>
+      </div>
+    </Wrapper>
+  )
+}
+
 export { NumberPadSection }
