4258810e3b9affc6776313198975f33c7661d49c
MongieLee <mongielee@163.com>
2020-06-09 10:58:14 +0800

    封装findTag到useTags里

--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useTags } from 'useTags'
+import { useParams } from 'react-router-dom'
+type Params = {
+  id: string
+}
 const TagEdit: React.FC = () => {
-    return (
-        <div>hi</div>
-    )
+  const { findTag } = useTags()
+  let { id } = useParams<Params>()
+  const tag = findTag(parseInt(id))
+  return <div>{tag.name}</div>
 }
 export { TagEdit }
