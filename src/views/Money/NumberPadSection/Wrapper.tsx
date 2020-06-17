d31f6d9dd7a3a0fac3d6b5efef3e045860b3c689
MongieLee <mongielee@163.com>
2020-06-17 23:38:22 +0800

    修改计算机样式

--- src/views/Money/NumberPadSection/Wrapper.tsx
+++ src/views/Money/NumberPadSection/Wrapper.tsx
@@ -1,20 +1,21 @@
 import styled from 'styled-components'
 const Wrapper = styled.section`
   .output {
-    background: #fff;
-    line-height: 72px;
-    font-size: 36px;
+    border-top: 1px #ddd solid;
+    background: rgb(242, 243, 245);
+    line-height: 1;
+    font-size: 50px;
     text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
-      inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+    padding: 5px 20px;
   }
   .numberPad {
     button {
-      font-size: 18px;
-      float: left;
-      width: 25%;
       height: 64px;
+      width: 25%;
+      border: 1px #ddd solid;
+      &:not(:last-child) {
+        background: rgb(242, 243, 245);
+      }
       &.zero {
         width: 50%;
       }
@@ -25,4 +26,4 @@ const Wrapper = styled.section`
     }
   }
 `
-export {Wrapper}
+export { Wrapper }
